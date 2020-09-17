7935d4ab25ff6545d34aaa67ca516e15ebfff315
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-09-17 19:47:26 +0800

    add App Component

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "assets/css/resets.css";
+import { MainApp } from "./style";
+import Nav from "components/Nav";
+
+export default function App() {
+    return (
+        <MainApp>
+            <Nav />
+        </MainApp>
+    );
+}
---
+++ src/components/App/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const MainApp = styled.div`
+    width: 100%;
+    max-width: 1000px;
+    background: white;
+    margin:auto;
+    height:100vh;
+`;
